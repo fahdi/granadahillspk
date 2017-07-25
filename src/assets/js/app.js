76a9be07155542d199bf019448a0fcfaa2470af8
Fahad Murtaza <fahdim@gmail.com>
2017-07-25 08:02:18 +0500

    First draft for the home page with nice header, footer and basic color scheme, the static content for Chairman's message has been added

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -6,7 +6,17 @@ window.$ = $;
 import Foundation from 'foundation-sites';
 // If you want to pick and choose which modules to include, comment out the above and uncomment
 // the line below
-//import './lib/foundation-explicit-pieces';
+// import './lib/foundation-explicit-pieces';
 
+$(function() {
+  $(window).scroll(function() {
+    var winTop = $(window).scrollTop();
+    if (winTop >= 30) {
+      $("body").addClass("sticky-shrinknav-wrapper");
+    } else{
+      $("body").removeClass("sticky-shrinknav-wrapper");
+    }
+  });
+});
 
 $(document).foundation();
